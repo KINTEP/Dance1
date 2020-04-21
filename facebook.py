09533d01e690ec077396b4e66adcc406ffdeb6f8
KINTEP <46703617+KINTEP@users.noreply.github.com>
2020-04-21 02:45:11 +0000

    pushed to heroku

--- facebook.py
+++ facebook.py
@@ -3,9 +3,9 @@ from flask import Flask, redirect, url_for
 from flask_dance.contrib.facebook import make_facebook_blueprint, facebook
 
 app = Flask(__name__)
-app.secret_key = os.environ.get("FLASK_SECRET_KEY", "supersekrit")
-app.config["FACEBOOK_OAUTH_CLIENT_ID"] = os.environ.get("FACEBOOK_OAUTH_CLIENT_ID")
-app.config["FACEBOOK_OAUTH_CLIENT_SECRET"] = os.environ.get("FACEBOOK_OAUTH_CLIENT_SECRET")
+app.secret_key = "FLASK_SECRET_KEY"
+app.config["FACEBOOK_OAUTH_CLIENT_ID"] = "663729490855447"
+app.config["FACEBOOK_OAUTH_CLIENT_SECRET"] = "aefe081cf32ab312dd3a637b2bac4b83"
 facebook_bp = make_facebook_blueprint()
 app.register_blueprint(facebook_bp, url_prefix="/login")
 
@@ -16,3 +16,6 @@ def index():
     resp = facebook.get("/me")
     assert resp.ok, resp.text
     return "You are {name} on Facebook".format(name=resp.json()["name"])
+
+if __name__ == '__main__':
+	app.run(debug = True)
